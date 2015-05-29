9e5605f6004abcc64bb9e246818cfc6f992e741f
Sonique <team.sonique.sns@bskyb.com>
2015-05-29 12:00:37 +0100

    TECH [Jon, Timur] Added functions for shoehorn goals

--- plugins/sonique/functions.zsh
+++ plugins/sonique/functions.zsh
@@ -88,12 +88,74 @@ function optimusprimer_deploy {
     ~/projects/assuranceview/optimusprimer-pipeline/src/test/shell/deploy-optimusprimer.sh $REVISION dev deploy
 }
 
-function optimusprimer_start {
-    /data/apps/optimusprimer/dev/start.sh
+ARTIFACTORY="http://repo.sns.sky.com:8081/artifactory"
+
+typeset -A ARTIFACT_COORDINATES
+ARTIFACT_COORDINATES[kiki]="sky.sns.kiki:kiki-core"
+ARTIFACT_COORDINATES[gruffalo]="sonique.gruffalo:gruffalo-build"
+ARTIFACT_COORDINATES[ffestiniog]="sonique.ffestiniog:ffestiniog-core"
+ARTIFACT_COORDINATES[spm-sat]="sonique.spm-sat:spm-sat-core"
+ARTIFACT_COORDINATES[redqueen]="sonique.redqueen:redqueen-core"
+ARTIFACT_COORDINATES[superman]="sky.sns:superman-deploy"
+ARTIFACT_COORDINATES[luthor]="sonique.luthor:luthor-core"
+
+function shoehorn-properties {
+    if [ -z "$2" ]
+    then
+        echo "usage: $1 application [version] [environment]"
+        return 1
+    else
+        APP="$2"
+    fi
+
+    if [ -z "$3" ]
+    then
+        coordinate=${ARTIFACT_COORDINATES[$APP]}
+
+        if [ -z ${coordinate} ]
+        then
+            groupId="sonique.${APP}"
+            artifactId="${APP}-deploy"
+        else
+            groupId=${coordinate%:*}
+            artifactId=${coordinate##*:}
+        fi
+
+        APP_VERSION=`curl -s "${ARTIFACTORY}/api/search/latestVersion?g=${groupId}&a=${artifactId}&repos=libs-releases"`
+        PROPERTIES_VERSION=`curl -s "${ARTIFACTORY}/api/search/latestVersion?g=${groupId}&a=${APP}-properties&repos=libs-releases"`
+        VERSION="${APP_VERSION}-${PROPERTIES_VERSION}"
+    else
+        VERSION="$3"
+    fi
+
+    if [ -z "$4" ]
+    then
+        ENV="dev"
+    else
+        ENV="$4"
+    fi
+
+    echo "-DapplicationName=${APP} -Dversion=${VERSION} -DenvironmentName=${ENV}"
 }
 
-function optimusprimer_stop {
-    /data/apps/optimusprimer/dev/stop.sh
+function deploy {
+    mvn shoehorn:deploy `shoehorn-properties deploy $@`
+}
+
+function start {
+    mvn shoehorn:start `shoehorn-properties start $@`
+}
+
+function stop {
+    mvn shoehorn:stop `shoehorn-properties stop $@`
+}
+
+function status {
+    mvn shoehorn:status `shoehorn-properties status $@`
+}
+
+function clean {
+    mvn shoehorn:clean `shoehorn-properties clean $@`
 }
 
 function setLinks {
