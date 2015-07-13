ac5959be764f56b54460b3cea43eabb3f421d462
Sonique <team.sonique.sns@bskyb.com>
2015-07-13 10:28:31 +0100

    TECH [ChrisH] Added openoffice

--- plugins/update/update_functions.zsh
+++ plugins/update/update_functions.zsh
@@ -46,7 +46,7 @@ function removeHomebrew() {
 }
 
 function removeNonBrewApplications() {
-    for app in 'Dropbox' 'Atom' 'Firefox' 'IntelliJ IDEA 14' 'TextMate' 'Google Chrome' 'iTerm' 'SourceTree' 'Microsoft Lync'; do
+    for app in 'Dropbox' 'Atom' 'Firefox' 'IntelliJ IDEA 14' 'TextMate' 'Google Chrome' 'iTerm' 'SourceTree' 'Microsoft Lync' 'OpenOffice'; do
 
         local appPath="/Applications/${app}.app"
         if [ -e ${appPath} ] && [ ! -h ${appPath} ]; then
