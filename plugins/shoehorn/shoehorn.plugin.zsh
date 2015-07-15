782a89a2ccdeeb4909695fb107aaf67ee6af2453
Sonique <team.sonique.sns@bskyb.com>
2015-07-15 17:10:43 +0100

    TECH [ChrisH, Timur] Moved compdefs into separate files

--- plugins/shoehorn/shoehorn.plugin.zsh
+++ plugins/shoehorn/shoehorn.plugin.zsh
@@ -9,4 +9,7 @@ _RESET_FORMATTING=$(tput sgr0)
 source "$(cd "$(dirname "$0")" && pwd)"/artifactory.zsh
 source "$(cd "$(dirname "$0")" && pwd)"/shoehorn_functions.zsh
 source "$(cd "$(dirname "$0")" && pwd)"/aggregates.zsh
-source "$(cd "$(dirname "$0")" && pwd)"/compdefs.zsh
+source "$(cd "$(dirname "$0")" && pwd)"/completion_helpers.zsh
+
+# :completion:function:completer:command:argument:tag
+zstyle ':completion:*:*:deploy:*:*' sort false
