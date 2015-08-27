06771354315842ab5f3372d4032dbc0a259ee4a4
Sonique <team.sonique.sns@bskyb.com>
2015-08-27 08:27:22 +0100

    TimW: sencha path for local config

--- plugins/sonique/exports.zsh
+++ plugins/sonique/exports.zsh
@@ -4,6 +4,6 @@ export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 export SVN_EDITOR=vim
 export CDPATH=.:~/projects:~/trunk/netstream
 export SHOEHORN_BUILD_DIR=/tmp/shoehorn
-export SENCHA_CMD_DIR=/Users/dev/bin/Sencha/Cmd/6.0.0.202
+export SENCHA_CMD_DIR=~/bin/Sencha/Cmd/6.0.0.202
 
 export PATH=.:$FIREFOX_HOME:$PATH:$SENCHA_CMD_DIR
