e3de4e15329bc2c17889002403d105b27e61b80d
Sonique <team.sonique.sns@bskyb.com>
2015-09-23 12:57:39 +0100

    TimW: changed update frequency to 7 days

--- plugins/update/update_functions.zsh
+++ plugins/update/update_functions.zsh
@@ -69,29 +69,28 @@ function timeSinceLastUpdate {
 }
 
 function checkForSoniqueEnvUpdates {
-    local dir=~/projects/sonique-env
-    local url="$(git -C ${dir} config --get remote.origin.url)"
+    local url="$(git -C ${ZDOTDIR} config --get remote.origin.url)"
     local remote_version="$(git ls-remote ${url} HEAD | awk '{print $1}')"
-    local local_version="$(git -C ${dir} rev-parse HEAD)"
+    local local_version="$(git -C ${ZDOTDIR} rev-parse HEAD)"
 
     local bold=$(tput bold)
     local text_yellow=$(tput setaf 3)
     local reset_formatting=$(tput sgr0)
 
     if [ ${remote_version} != ${local_version} ]; then
-        echo "${bold}${text_yellow}Your sonique-env is out of sync${reset_formatting}"
+        echo "${bold}${text_yellow}Your ${ZDOTDIR} is out of sync${reset_formatting}"
     fi
 }
 
 function startUpdateProcess {
-    local updateThresholdDays=${1:-1}
+    local updateThresholdDays=${1:-6}
     local day_seconds=$(expr 24 \* 60 \* 60)
     local update_frequency=$(expr ${day_seconds} \* ${updateThresholdDays})
     local time_since_update=$(timeSinceLastUpdate)
     local line
 
+    echo "It has been $(expr ${time_since_update} / ${day_seconds}) days since your environment was updated"
     if [ ${time_since_update} -gt ${update_frequency} ]; then
-        echo "It has been $(expr ${time_since_update} / ${day_seconds}) days since your environment was updated"
         echo "Would you like to check for updates? [Y/n]: \c"
         read line
         if [ "${line}" = Y ] || [ "${line}" = y ]; then
