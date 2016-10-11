602eb9082e0489d07c429752121ac57893b6f017
Sonique <team.sonique.sns@bskyb.com>
2016-10-11 11:05:18 +0100

    TECH [Sam,Pete] fixed equality operator

--- plugins/sonique/kubectl_auto_complete.zsh
+++ plugins/sonique/kubectl_auto_complete.zsh
@@ -2,8 +2,9 @@ current_zsh_version=$(zsh --version | awk '{print $2}')
 expected_zsh_version=5.2
 version_is_less_than=$(bc <<< "$current_zsh_version < $expected_zsh_version")
 
-if [ "$version_is_less_than" == 1 ]; then
-brew install zsh --force
+if [ "$version_is_less_than" -eq 1 ]; 
+then
+    brew install zsh --force
 fi
 
 kubectl_version=$(kubectl version --client | grep master) > /dev/null 
