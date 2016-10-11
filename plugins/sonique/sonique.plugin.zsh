d39053142190771389e1d12148d889296c7ddf4b
Sonique <team.sonique.sns@bskyb.com>
2016-10-11 14:17:01 +0100

    TECH [Sam] commented kubectl autocomplete plugin until it's fixed

--- plugins/sonique/sonique.plugin.zsh
+++ plugins/sonique/sonique.plugin.zsh
@@ -3,7 +3,7 @@ setopt INTERACTIVECOMMENTS
 
 source "$(cd "$(dirname "$0")" && pwd)"/exports.zsh
 source "$(cd "$(dirname "$0")" && pwd)"/aliases.zsh
-source "$(cd "$(dirname "$0")" && pwd)"/kubectl_auto_complete.zsh
+#source "$(cd "$(dirname "$0")" && pwd)"/kubectl_auto_complete.zsh
 source "$(cd "$(dirname "$0")" && pwd)"/functions.zsh
 
 goJava8
